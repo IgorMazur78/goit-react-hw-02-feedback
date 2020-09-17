a5f39f59edcfaba666245130b3d5e5abfe3ece2b
IgorMazur78 <spanishman0605@gmail.com>
2020-09-17 16:51:32 +0300

    Updates

---
+++ precache-manifest.198bbf34dbef2c4914ebfa6c942d5009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610f946f441d1ae5d2ef60e2ead445f1",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "0468ee4129c6c71bd4b1",
+    "url": "/goit-react-hw-02-feedback/static/css/main.887372d2.chunk.css"
+  },
+  {
+    "revision": "04010963450733e3144c",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c73624e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c73624e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0468ee4129c6c71bd4b1",
+    "url": "/goit-react-hw-02-feedback/static/js/main.255177d9.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
