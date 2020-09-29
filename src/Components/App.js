90d663715d7e7247f55874451383f0a3ed8529db
IgorMazur78 <spanishman0605@gmail.com>
2020-09-29 17:48:01 +0300

    six

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,32 +10,39 @@ export default class App extends Component {
     neutral: 0,
     bad: 0,
   };
+  Counter = (type) => {
+    this.setState((prevState) => {
+      return {
+        [type]: prevState[type] + 1,
+      };
+    });
+  };
 
   
-  CounterGood = () => {
-    this.setState((prevState) => {
-      return {
-        good: prevState.good + 1,
-      };
-    });
-  };
+  // CounterGood = () => {
+  //   this.setState((prevState) => {
+  //     return {
+  //       good: prevState.good + 1,
+  //     };
+  //   });
+  // };
 
 
-  CounterNeutral = () => {
-    this.setState((prevState) => {
-      return {
-        neutral: prevState.neutral + 1,
-      };
-    });
-  };
+  // CounterNeutral = () => {
+  //   this.setState((prevState) => {
+  //     return {
+  //       neutral: prevState.neutral + 1,
+  //     };
+  //   });
+  // };
 
-  CounterBad = () => {
-    this.setState((prevState) => {
-      return {
-        bad: prevState.bad + 1,
-      };
-    });
-  };
+  // CounterBad = () => {
+  //   this.setState((prevState) => {
+  //     return {
+  //       bad: prevState.bad + 1,
+  //     };
+  //   });
+  // };
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
     return good + bad + neutral
@@ -57,9 +64,9 @@ export default class App extends Component {
     return (
       <div>
         <Feedback
-          onCounterGood={this.CounterGood}
-          onCounterNeutral={this.CounterNeutral}
-          onCounterBad={this.CounterBad}
+          onCounter={this.Counter}
+          // onCounterNeutral={this.Counter}
+          // onCounterBad={this.Counter}
         />
 {this.countTotalFeedback() === 0 && (
           <div>
@@ -81,25 +88,7 @@ export default class App extends Component {
         statPositive={this.countPositiveFeedbackPercentage()}
       />}
 
-        {/* {this.countTotalFeedback() === 0 ? (
-          <div>
-          <Title />
-          <Statistic
-            statGood={good}
-            statNeutral={neutral}
-            statBad={bad}
-            statTotal={this.countTotalFeedback()}
-            statPositive={this.countPositiveFeedbackPercentage()}/>
-            </div>
-        ) : (
-          <Statistic
-            statGood={good}
-            statNeutral={neutral}
-            statBad={bad}
-            statTotal={this.countTotalFeedback()}
-            statPositive={this.countPositiveFeedbackPercentage()}
-          />
-        )} */}
+        
       </div>
     );
   }
--- src/Components/Feedback/blockFeedback.js
+++ src/Components/Feedback/blockFeedback.js
@@ -2,19 +2,19 @@ import React from "react";
 
 import style from "./feedback.module.css"
 
-const Feedback = ({onCounterGood,onCounterNeutral,onCounterBad}) => {
+const Feedback = ({onCounter}) => {
   return (
   <div className = {style.feedback}>
       <h2 className = {style.titleFeedback}>Please leave feedback</h2>
       <ul className = {style.feedbackList}>
         <li>
-          <button className = {style.buttonFeedback} type ="button" onClick = {onCounterGood}>Good</button>
+          <button className = {style.buttonFeedback} type ="button" onClick = {() => onCounter("good")}>Good</button>
         </li>
         <li>
-          <button className = {style.buttonFeedback} type ="button" onClick = {onCounterNeutral}>Neutral</button>
+          <button className = {style.buttonFeedback} type ="button" onClick = {() => onCounter("neutral")}>Neutral</button>
         </li>
         <li>
-          <button className = {style.buttonFeedback} type ="button" onClick = {onCounterBad}>Bad</button>
+          <button className = {style.buttonFeedback} type ="button" onClick = {() => onCounter("bad")}>Bad</button>
         </li>
       </ul>
       </div>
